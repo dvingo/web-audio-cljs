f5ec3b29db4fa5acf1feff45e964e15821593d5d
Dan Vingo <danvingo@gmail.com>
2015-05-12 02:22:05 -0400

    Added build-button macro

--- src/web_audio_cljs/components/tracks.cljs
+++ src/web_audio_cljs/components/tracks.cljs
@@ -3,19 +3,16 @@
             [om.dom :as dom :include-macros true]
             [web-audio-cljs.state :refer [tracks]]
             [web-audio-cljs.utils :refer [make-button]]
-            [web-audio-cljs.components.track :refer [track-view]]
-            [cljs.core.async :refer [put!]]))
+            [web-audio-cljs.components.track :refer [track-view]])
+  (:require-macros [web-audio-cljs.macros :refer [send! build-button]]))
 
 (defn tracks-view [_ owner]
   (reify
     om/IDisplayName (display-name [_] "tracks-view")
     om/IRender
     (render [_]
-      (let [trcks (om/observe owner (tracks))
-            new-track-btn (make-button "new-track-button"
-                            #(put! (:action-chan (om/get-shared owner))
-                                   [:make-new-track]) "New Track")]
+      (let [trcks (om/observe owner (tracks))]
         (dom/div nil
-          (om/build new-track-btn nil)
+          (build-button "new-track-button" #(send! owner :make-new-track) "New Track")
           (apply dom/div #js {:className "tracks"}
             (map #(om/build track-view %) trcks)))))))
--- src/web_audio_cljs/macros.clj
+++ src/web_audio_cljs/macros.clj
@@ -3,3 +3,15 @@
 (defmacro send! [owner action & data]
   `(cljs.core.async/put! (:action-chan (om.core/get-shared ~owner))
      [~action ~@data]))
+
+
+(defmacro build-button [disp-name on-click label]
+  `(om.core/build
+     (fn [~(symbol "_") ~(symbol "owner")]
+       (reify
+         om.core/IDisplayName
+         (~(symbol "display-name") [~(symbol "_")] ~disp-name)
+         om.core/IRender
+         (~(symbol "render") [~(symbol "_")]
+           (om.dom/button (cljs.core/js-obj "onClick" ~on-click) ~label))))
+     nil))
